6bb1302ae258980ed1a4534ab3c7d8fdb7035511
Kieyyy <kjbello@usa.edu.ph>
2024-11-30 18:41:42 +0800

    getPatientData.js

--- getPatientData.js
+++ getPatientData.js
@@ -1,8 +1,7 @@
-// getPatientData.js
+
 const fs = require('fs');
 const path = require('path');
 
-// Function to get patient data based on interval
 function getPatientData(patientId, interval, date) {
   const folderPath = path.join(__dirname, 'patient-history', patientId, interval);
   const files = fs.readdirSync(folderPath);
@@ -13,6 +12,5 @@ function getPatientData(patientId, interval, date) {
   });
 }
 
-// Example usage: Get data for patient "manuel-garcia" for 1-hour history on November 24, 2024
 const patientRecords = getPatientData('manuel-garcia', 'history-1hr', '2024-11-24');
 console.log(patientRecords);
